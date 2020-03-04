5d169a01881f62995dafac63d62d261ee378378c
Carlos <carloskrlyn@gmail.com>
2020-03-03 21:47:43 -0300

    Alterações na pj_senac

--- estoque/resources/views/componente/index.blade.php
+++ estoque/resources/views/componente/index.blade.php
@@ -37,7 +37,7 @@
         {{ session()->get('success') }}
       </div><br />
     @endif
-    <a href="{{ route('componente.create')}}" class="btn btn-primary">NOVO componente</a>
+    <a href="{{ route('componente.create')}}" class="btn btn-primary">Lista de docentes</a>
     <table class="table table-striped table-bordered">
       <thead>
           <tr style="text-align: center !important;">
@@ -74,5 +74,104 @@
             @endforeach
         </tbody>
     </table>
+
+
+    <div class="uper">
+        @if(session()->get('success'))
+          <div class="alert alert-success">
+            {{ session()->get('success') }}
+          </div><br />
+        @endif
+        <a href="{{ route('componente.create')}}" class="btn btn-primary">Lista de cursos</a>
+        <table class="table table-striped table-bordered">
+          <thead>
+              <tr style="text-align: center !important;">
+                <th>ID</th>
+                <th>NOME componente</th>
+                <th>DESCRIÇÃO</th>
+                <th>QUANTIDADE DE ALUNOS</th>
+                <th>DATA INÍCIO</th>
+                <th>DATA FIM</th>
+                <th>CARGA HORÁRIA</th>
+                <th colspan="2">AÇÃO</th>
+              </tr>
+          </thead>
+            <tbody>
+                @foreach($componentes as $componente)
+                <tr>
+                    <th style="text-align: center !important;">{{$componente->id_componente}}</th>
+                    <td>{{$componente->no_componente}}</td>
+                    <td>{{$componente->descricao}}</td>
+                    <td>{{$componente->qt_alunos}}</td>
+                    <td>{{$componente->dt_inicio}}</td>
+                    <td>{{$componente->dt_fim}}</td>
+                    <td>{{$componente->qt_horas_componente}}</td>
+
+                    <td><a href="{{ route('componente.edit',$componente->id_componente)}}" class="btn btn-primary btn-block">Alterar</a></td>
+                    <td>
+                        <form action="{{ route('componente.destroy', $componente->id_componente)}}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger btn-block" type="submit">Excluir</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+      <div>
+
+
+
+
+
+        <div class="uper">
+            @if(session()->get('success'))
+              <div class="alert alert-success">
+                {{ session()->get('success') }}
+              </div><br />
+            @endif
+            <a href="{{ route('componente.create')}}" class="btn btn-primary">Lista de materiais</a>
+            <table class="table table-striped table-bordered">
+              <thead>
+                  <tr style="text-align: center !important;">
+                    <th>ID</th>
+                    <th>NOME componente</th>
+                    <th>DESCRIÇÃO</th>
+                    <th>QUANTIDADE DE ALUNOS</th>
+                    <th>DATA INÍCIO</th>
+                    <th>DATA FIM</th>
+                    <th>CARGA HORÁRIA</th>
+                    <th colspan="2">AÇÃO</th>
+                  </tr>
+              </thead>
+                <tbody>
+                    @foreach($componentes as $componente)
+                    <tr>
+                        <th style="text-align: center !important;">{{$componente->id_componente}}</th>
+                        <td>{{$componente->no_componente}}</td>
+                        <td>{{$componente->descricao}}</td>
+                        <td>{{$componente->qt_alunos}}</td>
+                        <td>{{$componente->dt_inicio}}</td>
+                        <td>{{$componente->dt_fim}}</td>
+                        <td>{{$componente->qt_horas_componente}}</td>
+
+                        <td><a href="{{ route('componente.edit',$componente->id_componente)}}" class="btn btn-primary btn-block">Alterar</a></td>
+                        <td>
+                            <form action="{{ route('componente.destroy', $componente->id_componente)}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger btn-block" type="submit">Excluir</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+          <div>
+
+
+
+
   <div>
 @endsection
